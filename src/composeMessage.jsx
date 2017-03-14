c4ffc7afee17571def42028a51cc1fcfe3efbe98
Myles Rademaker <mylesprademaker@gmail.com>
2017-03-14 13:31:04 -0700

    Created text area and handler functions

--- src/composeMessage.jsx
+++ src/composeMessage.jsx
@@ -3,12 +3,29 @@ import React, {Component} from 'react';
 class ComposeMessage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(input) {
+    this.props.postMessage(input);
+    this.setState({value: ''});
   }
 
   render() {
     return (
       <div>
-        <h1>'TEST'</h1>
+        <textarea value={this.state.value} onChange={this.handleChange} placeholder='Whats on your mind?'></textarea>
+        <div onClick={() => {this.handleSubmit(this.state.value)}}>
+          Submit
+        </div>
       </div>
     );
   }
--- src/message.jsx
+++ src/message.jsx
@@ -12,7 +12,7 @@ class Message extends Component {
         <p>{this.props.id}</p>
         <p>{moment(this.props.created_at).format('MMMM Do YYYY, h:mm:ss a')}</p>
         <h1>{this.props.text}</h1>
-        <div onClick={() =>{this.props.deleteMessage(this.props.id)}}>
+        <div onClick={() => {this.props.deleteMessage(this.props.id)}}>
           DELETE
         </div>
       </div>
